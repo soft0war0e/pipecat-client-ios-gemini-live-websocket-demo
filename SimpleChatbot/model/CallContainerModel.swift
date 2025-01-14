c5589034e3e2a58c0c762d818ba98ecc811dc015
Paul Kompfner <paul@daily.co>
2025-01-14 11:25:01 -0500

    Wire up live updates to the selected mic and available mics list. This is beneficial for a few reasons: - Live updates are nice! We can now more easily see what's going on when we connect or disconnect a mic. - Resolves an issue where the initial selected mic was not shown. - Let us see when the Pipecat client automatically switches to a new mic, like when one is connected.

--- SimpleChatbot/model/CallContainerModel.swift
+++ SimpleChatbot/model/CallContainerModel.swift
@@ -21,6 +21,16 @@ class CallContainerModel: ObservableObject {
     
     var rtviClientIOS: RTVIClient?
     
+    @Published var selectedMic: MediaDeviceId? = nil {
+        didSet {
+            guard let selectedMic else { return } // don't store nil
+            var settings = SettingsManager.getSettings()
+            settings.selectedMic = selectedMic.id
+            SettingsManager.updateSettings(settings: settings)
+        }
+    }
+    @Published var availableMics: [MediaDeviceInfo] = []
+    
     init() {
         // Changing the log level
         RTVIClientIOS.setLogLevel(.warn)
@@ -68,14 +78,18 @@ class CallContainerModel: ObservableObject {
         )
         self.rtviClientIOS?.delegate = self
         self.rtviClientIOS?.start() { result in
-            if case .failure(let error) = result {
+            switch result {
+            case .failure(let error):
                 self.showError(message: error.localizedDescription)
                 self.rtviClientIOS = nil
+            case .success():
+                // Apply initial mic preference
+                if let selectedMic = currentSettings.selectedMic {
+                    self.selectMic(MediaDeviceId(id: selectedMic))
+                }
+                // Populate available devices list
+                self.availableMics = self.rtviClientIOS?.getAllMics() ?? []
             }
-        }
-        // Selecting the mic based on the preferences
-        if let selectedMic = currentSettings.selectedMic {
-            self.rtviClientIOS?.updateMic(micId: MediaDeviceId(id:selectedMic), completion: nil)
         }
         self.saveCredentials(geminiAPIKey: geminiAPIKey)
     }
@@ -112,6 +126,12 @@ class CallContainerModel: ObservableObject {
         currentSettings.geminiAPIKey = geminiAPIKey
         // Saving the settings
         SettingsManager.updateSettings(settings: currentSettings)
+    }
+    
+    @MainActor
+    func selectMic(_ mic: MediaDeviceId) {
+        self.selectedMic = mic
+        self.rtviClientIOS?.updateMic(micId: mic, completion: nil)
     }
 }
 
@@ -170,6 +190,18 @@ extension CallContainerModel:RTVIClientDelegate, LLMHelperDelegate {
     func onUserAudioLevel(level: Float) {
         Task { @MainActor in
             self.localAudioLevel = level
+        }
+    }
+    
+    func onAvailableMicsUpdated(mics: [MediaDeviceInfo]) {
+        Task { @MainActor in
+            self.availableMics = mics
+        }
+    }
+    
+    func onMicUpdated(mic: MediaDeviceInfo?) {
+        Task { @MainActor in
+            self.selectedMic = mic?.id
         }
     }
 }
